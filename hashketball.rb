d6d40c6b517d7e7d6316679d25f83ae00a1cab7a
Stephanie Messad <sabud1208@gmail.com>
2018-12-12 01:43:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,36 @@
 # Write your code here!
+require "pry"
+def game_hash
+  game_hash= {
+    :home=> {:team_name=>" ", :colors => [],
+      :players=>{
+        number:" ",
+        shoe:" ",
+        points:" ",
+        rebounds:" ",
+        assists:" ",
+        steals: " ",
+        blocks:" ",
+        slam_dunks:" ",
+        }
+      } ,
+    :away=> {:team_name=>" ", :colors => [],
+      :players=>{
+        number:" ",
+        shoe:" ",
+        points:" ",
+        rebounds:" ",
+        assists:" ",
+        steals: " ",
+        blocks:" ",
+        slam_dunks:" ",
+        }
+      }
+    }
+end
 
+def num_points_scored(player)
+   game_hash[:away][:players][:points]
+ end
 
-
-
-
-
-
-
-
-
+puts num_points_scored("Jeff Adrien")
